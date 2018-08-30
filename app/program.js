536abd3b61fad4b57ac75e25559a27201fc3d7c6
Miro Dresler <djmirq@wp.pl>
2018-08-30 19:27:41 +0200

    Fix: program.js format

--- app/program.js
+++ app/program.js
@@ -4,9 +4,9 @@ var toHHMMss = require('../modules/secToTime');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    if(input !== null) {
+    if (input !== null) {
         var instruction = input.trim();
-        switch(instruction) {
+        switch (instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
                 process.exit();
@@ -14,17 +14,16 @@ process.stdin.on('readable', function() {
             case '/sayhello':
                 process.stdout.write('hello!\n');
                 break;
-			case '/getOSinfo':
-				OSinfo.print();
-				break;
+            case '/getOSinfo':
+                OSinfo.print();
+                break;
             default:
-				if (isNaN(input)) {
-					process.stderr.write('Wrong process name or number!\n');
-				} else {
-					process.stdout.write(toHHMMss.toHHMMss(input) +'\n');
-				};
-				break;
+                if (isNaN(input)) {
+                    process.stderr.write('Wrong process name or number!\n');
+                } else {
+                    process.stdout.write(toHHMMss.toHHMMss(input) + '\n');
+                };
+                break;
         };
     }
 });
- 
